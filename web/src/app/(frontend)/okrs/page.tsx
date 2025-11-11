fcf8e110284540dbe386c3e30a1e3ba2604b5238
JoaoPedroBPG <joaopedrogunthen@gmail.com>
2025-11-11 20:19:00 -0300

    Solving Little Bug

--- web/src/app/(frontend)/okrs/page.tsx
+++ web/src/app/(frontend)/okrs/page.tsx
@@ -1,24 +1,24 @@
-"use client";
-import { useState, useEffect } from "react";
-import { OkrHeader } from "./OkrHeader";
+// "use client";
+// import { useState, useEffect } from "react";
+// import { OkrHeader } from "./OkrHeader";
 
-export default function OkrsPage() {
-  const [okrs, setOkrs] = useState<Okrs[]>([]);
-  const [period, setPeriod] = useState<string>("Q1 2024");
+// export default function OkrsPage() {
+//   const [okrs, setOkrs] = useState<Okrs[]>([]);
+//   const [period, setPeriod] = useState<string>("Q1 2024");
 
-  useEffect(() => {
-    (async () => {
-      try {
-        setOkrs(await fetchOkrs());
-      } catch (e: any) {
-        console.error(e);
-      }
-    })();
-  },
-  return (
-    <OkrHeader okrs={okrs}
-            year={year}
-            period={period}
-            onChangePeriod={setPeriod}/>
-  )
-}
+//   useEffect(() => {
+//     (async () => {
+//       try {
+//         setOkrs(await fetchOkrs());
+//       } catch (e: any) {
+//         console.error(e);
+//       }
+//     })();
+//   },
+//   return (
+//     <OkrHeader okrs={okrs}
+//             year={year}
+//             period={period}
+//             onChangePeriod={setPeriod}/>
+//   )
+// }
